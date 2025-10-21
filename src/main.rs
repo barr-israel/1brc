31d26fdcfdfc9553816ec27cabaa2b18e528fdea
Barr <barr@barrisrael.com>
2025-10-22 00:01:02 +0300

    change main solution

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::io::Read;
 #[global_allocator]
 static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-mod compliant_multi_thread;
+mod final_multi_thread;
 
 fn main() {
     let (mut reader, writer) = std::io::pipe().unwrap();
     if unsafe { libc::fork() } == 0 {
-        compliant_multi_thread::run(writer);
+        final_multi_thread::run(writer);
     } else {
         _ = reader.read_exact(&mut [0u8]);
     }
