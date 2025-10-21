93b41a2f5307ebbe40f93e4231e008fb6383cc48
Barr <barr@barrisrael.com>
2025-10-21 23:10:37 +0300

    final_multi_thread

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::io::Read;
 #[global_allocator]
 static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
-mod use_avx512;
+mod final_multi_thread;
 
 fn main() {
     let (mut reader, writer) = std::io::pipe().unwrap();
     if unsafe { libc::fork() } == 0 {
-        use_avx512::run(writer);
+        final_multi_thread::run(writer);
     } else {
         _ = reader.read_exact(&mut [0u8]);
     }
