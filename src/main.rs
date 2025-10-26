6f52d8f5f81f7f156e0e7d4c22de0dc187dcc5d7
Barr <barr@barrisrael.com>
2025-10-27 00:45:03 +0200

    prefetching fails

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,13 @@ static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 use std::io::Read;
 
 mod my_phf;
-mod prefetch;
 mod station_names;
+mod use_phf;
 
 fn main() {
     let (mut reader, writer) = std::io::pipe().unwrap();
     if unsafe { libc::fork() } == 0 {
-        prefetch::run(writer);
+        use_phf::run(writer);
     } else {
         _ = reader.read_exact(&mut [0u8]);
     }
