63098c5fb998392cd612b32b7867d09829ee4d14
Barr <barr@barrisrael.com>
2025-11-04 17:36:07 +0200

    test ipl buff

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ static GLOBAL: jemallocator::Jemalloc = jemallocator::Jemalloc;
 
 use std::io::Read;
 
+mod ilp;
 mod my_phf;
 mod station_names;
-mod use_phf;
 
 fn main() {
     let (mut reader, writer) = std::io::pipe().unwrap();
     if unsafe { libc::fork() } == 0 {
-        use_phf::run(writer);
+        ilp::run(writer);
     } else {
         _ = reader.read_exact(&mut [0u8]);
     }
